3982caf8130908433f1ec5e8bdcdf6325366e627
Rajkamal <rajkamal.smit@gmail.com>
2021-03-31 13:01:17 +0530

    empty cart condition added

--- src/Checkout/Checkout.js
+++ src/Checkout/Checkout.js
@@ -21,47 +21,59 @@ function Checkout() {
           className="checkout__headerImg"
           src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg"
         />
-        <div className="checkout__header_top">Hello,</div>
-        <div className="checkout__subheading">Your shopping Basket</div>
 
-        {basket.map((product) => {
-          return (
-            <div className="checkout_products">
-              <div className="checkout_product">
-                <div className="checkout_product_image_div">
-                  <img className="checkout_product_image" src={product.image} />
-                </div>
-                <div className="checkout_product_description">
-                  <div className="checkout_product_description_title">
-                    {product.title}
-                  </div>
+        {basket.length == 0 ? (
+          <div>
+            <h2> Your shopping cart is empty</h2>
+          </div>
+        ) : (
+          <div>
+            <div className="checkout__header_top">Hello,</div>
+            <div className="checkout__subheading">Your shopping Basket</div>
 
-                  <div style={{ marginBottom: "5px" }}>
-                    $<strong>{product.price}</strong>
-                  </div>
-                  <div
-                    className="product_rating"
-                    style={{ marginBottom: "5px" }}
-                  >
-                    {Array(product.rating)
-                      .fill()
-                      .map((_, i) => (
-                        <p>🌟</p>
-                      ))}
-                  </div>
-                  <div>
-                    <button
-                      className="product_button"
-                      onClick={() => handleRemove(product)}
-                    >
-                      Remove from Basket
-                    </button>
+            {basket.map((product) => {
+              return (
+                <div className="checkout_products">
+                  <div className="checkout_product">
+                    <div className="checkout_product_image_div">
+                      <img
+                        className="checkout_product_image"
+                        src={product.image}
+                      />
+                    </div>
+                    <div className="checkout_product_description">
+                      <div className="checkout_product_description_title">
+                        {product.title}
+                      </div>
+
+                      <div style={{ marginBottom: "5px" }}>
+                        $<strong>{product.price}</strong>
+                      </div>
+                      <div
+                        className="product_rating"
+                        style={{ marginBottom: "5px" }}
+                      >
+                        {Array(product.rating)
+                          .fill()
+                          .map((_, i) => (
+                            <p>🌟</p>
+                          ))}
+                      </div>
+                      <div>
+                        <button
+                          className="product_button"
+                          onClick={() => handleRemove(product)}
+                        >
+                          Remove from Basket
+                        </button>
+                      </div>
+                    </div>
                   </div>
                 </div>
-              </div>
-            </div>
-          );
-        })}
+              );
+            })}
+          </div>
+        )}
       </div>
 
       <div className="checkout__right">
